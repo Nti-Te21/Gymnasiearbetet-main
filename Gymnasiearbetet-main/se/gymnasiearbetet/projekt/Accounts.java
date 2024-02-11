e2a86693251fc3c17fb01eab0ac2a4e8b10e1809
Nti-Te21 <alec.andin@elev.ga.ntig.se>
2024-02-11 20:21:31 +0100

    java step 8 for real only important thing i belive

--- Gymnasiearbetet-main/se/gymnasiearbetet/projekt/Accounts.java
+++ Gymnasiearbetet-main/se/gymnasiearbetet/projekt/Accounts.java
@@ -2,15 +2,21 @@ package se.gymnasiearbetet.projekt;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Base64;
+import java.util.HashMap;
 
 public class Accounts {
     private String username;
     private String password;
-    private Integer accountId;
+    private String accountId;
+    public static List<String> currentAccountRecords = new ArrayList<>();
+    public static Map<String, List<String>> localRecordsData = new HashMap<>();
 
-    private Accounts(String username, String password, Integer accountId) {
+    private Accounts(String username, String password, String accountId) {
         this.username = username;
         this.password = password;
         this.accountId = accountId;
@@ -23,6 +29,7 @@ public class Accounts {
             System.out.println("Password:");
             var password = scanner.nextLine();
             var accountId = account_list.size() + 1;
+            var accountIdStr = String.valueOf(accountId);
             boolean nameTaken = false;
             for (Accounts currentAccount : account_list) {
                 if (username.equals(currentAccount.username)) {
@@ -32,9 +39,22 @@ public class Accounts {
                 }
             }
             if (!nameTaken) {
-                System.out.println(accountId);
-                addAccountToFile(new Accounts(username, password, accountId));
-                return new Accounts(username, password, accountId);
+                addAccountToFile(new Accounts(username, password, accountIdStr));
+                return new Accounts(username, password, accountIdStr);
+            }
+        }
+    }
+
+    public static void importRecordsFromFile() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("records.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length >= 2) {
+                    String accountId = parts[0];
+                    List<String> records = new ArrayList<>(Arrays.asList(parts));
+                    localRecordsData.put(accountId, records);
+                }
             }
         }
     }
@@ -66,7 +86,7 @@ public class Accounts {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] old_account = line.split(",");
-                account_list.add(new Accounts(old_account[0], old_account[1], Integer.parseInt(old_account[2])));
+                account_list.add(new Accounts(old_account[0], old_account[1], (old_account[2])));
 
             }
         }
@@ -76,6 +96,99 @@ public class Accounts {
     public static void addAccountToFile(Accounts account) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("accounts.csv", true))) {
             writer.write(account.username + "," + account.password + "," + account.accountId + "\n");
+        }
+    }
+
+    public static void passwordManagment(Scanner scanner, Accounts loggedInAccount) throws IOException {
+        boolean done = false;
+        localRecordsData.clear();
+        currentAccountRecords.clear();
+        importRecordsFromFile();
+        currentAccountRecords = localRecordsData.get(loggedInAccount.accountId);
+        if (currentAccountRecords == null) {
+            currentAccountRecords = new ArrayList<>();
+        }
+        while (!done) {
+            System.out.println(
+                    "\n1. Create stored password \n2. View passwords\n3. Log out \n4. Exit program and log out\n");
+            var choice = scanner.nextLine();
+            if (choice.equals("1")) {
+                createRecord(scanner, loggedInAccount);
+            } else if (choice.equals("2")) {
+                System.out.println("\nViewing passwords coming soon");
+            } else if (choice.equals("3")) {
+                System.out.println("\nLogged out!");
+                done = true;
+                saveRecordsToFile();
+            } else if (choice.equals("4")) {
+                System.out.println("\nLogged out! See you next time!\n");
+                done = true;
+                saveRecordsToFile();
+                System.exit(0);
+            }
+
+        }
+    }
+
+    public static void createRecord(Scanner scanner, Accounts loggedInAccount) throws IOException {
+        System.out.println("Record name:");
+        var recordName = scanner.nextLine();
+        System.out.println("Username:");
+        var username = scanner.nextLine();
+        System.out.println("Password:");
+        var password = scanner.nextLine();
+        var record = new StringBuffer();
+        record.append(recordName).append("\n");
+        record.append(username).append("\n");
+        record.append(password);
+        String stringRecord = record.toString();
+        byte[] byteRecord = stringRecord.getBytes();
+        var base64Record = Base64.getEncoder().encodeToString(byteRecord);
+        String fullRecord = new String("," + base64Record);
+        if (currentAccountRecords.contains(loggedInAccount.accountId)) {
+            System.out.println(currentAccountRecords + "currentAccountRecords during create on if");
+            currentAccountRecords.add(fullRecord);
+        } else {
+            System.out.println(currentAccountRecords + "currentAccountRecords during createon else");
+            currentAccountRecords.add(loggedInAccount.accountId);
+            currentAccountRecords.add(fullRecord);
+        }
+    }
+
+    public static void saveRecordsToFile() throws IOException {
+        System.err.println(localRecordsData + "localRecordsstart");
+        System.err.println(currentAccountRecords + "currentAccountRecords1");
+        try {
+            System.out.println(localRecordsData + "local data before put");
+            if (!localRecordsData.containsKey(currentAccountRecords.get(0))) {
+                System.out.println("does not contain key");
+                localRecordsData.put(currentAccountRecords.get(0), currentAccountRecords);
+            }
+            System.out.println(localRecordsData + "local data after put");
+        } catch (Exception e) {
+            System.err.println("error index not there");
+        }
+        if (currentAccountRecords.isEmpty() != true) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("records.csv"))) {
+                if (localRecordsData != null) {
+                    boolean firstAccount = true;
+                    for (String accountId : localRecordsData.keySet()) {
+                        if (!firstAccount) {
+                            writer.write("\n");
+                        } else {
+                            firstAccount = false;
+                        }
+                        List<String> records = localRecordsData.get(accountId);
+                        System.out.println(records + "records from local data based on id pre write");
+                        if (records != null) {
+                            for (String record : records) {
+                                writer.write(record + ",");
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
--- Gymnasiearbetet-main/se/gymnasiearbetet/projekt/Project.java
+++ Gymnasiearbetet-main/se/gymnasiearbetet/projekt/Project.java
@@ -23,35 +23,12 @@ public class Project {
                 } else if (choice.equals("2")) {
                     var loggedInAccount = Accounts.log_in(scanner, account_list);
                     if (loggedInAccount != null) {
-                        passwordManagment(scanner, loggedInAccount);
+                        Accounts.passwordManagment(scanner, loggedInAccount);
                     }
                 } else if (choice.equals("3")) {
                     done = true;
                 }
             }
-        }
-
-    }
-
-    private static void passwordManagment(Scanner scanner, Accounts loggedInAccount) {
-        boolean done = false;
-        while (!done) {
-            System.out.println(
-                    "\n1. Create stored password \n2. View passwords\n3. Log out \n4. Exit program and log out\n");
-            var choice = scanner.nextLine();
-            if (choice.equals("1")) {
-                System.out.println("\nCreating password coming soon");
-            } else if (choice.equals("2")) {
-                System.out.println("\nViewing passwords coming soon");
-            } else if (choice.equals("3")) {
-                System.out.println("\nLogged out!");
-                done = true;
-            } else if (choice.equals("4")) {
-                System.out.println("\nLogged out! See you next time!\n");
-                done = true;
-                System.exit(0);
-            }
-
         }
     }
 }
